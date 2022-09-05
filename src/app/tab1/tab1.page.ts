d0bb8227e974919643250002d16756f2b18b1031
Libena <li.jara@duocuc.cl>
2022-09-04 23:20:30 -0300

    animacion

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { AnimationController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  @ViewChild('loadingIcon', { read: ElementRef }) loadingIcon: ElementRef;
+  @ViewChild('cartBtn', { read: ElementRef }) cartBnt: ElementRef;
+  @ViewChild('cartFabBtn', { read: ElementRef }) cartFabBnt: ElementRef;
 
-  constructor() {}
+  constructor(private animationCtrl: AnimationController) {}
+  startLoad() {
+    const loadingAnimation = this.animationCtrl.create('loading-animation')
+      .addElement(this.loadingIcon.nativeElement)
+      .duration(1500)
+      .iterations(3)
+      .fromTo('transform', 'rotate(0deg)', 'rotate(360deg)');
 
+    // Don't forget to start the animation!
+    loadingAnimation.play();
+}
 }
